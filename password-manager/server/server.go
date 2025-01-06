8cf0e7864938d5abc026b7b440678d035e48b3dc
vviveksharma <visharma@progress.com>
2025-01-06 17:31:38 +0530

    Added auto generation of the latest master
    Signed-off-by: vviveksharma <visharma@progress.com>

--- password-manager/server/server.go
+++ password-manager/server/server.go
@@ -1,8 +1,10 @@
 package server
 
 import (
+	"errors"
 	"fmt"
 	"log"
+	dallayer "password-manager/dalLayer"
 	"password-manager/db"
 	"password-manager/handlers"
 	"password-manager/services"
@@ -28,8 +30,38 @@ func GetDbCheck() {
 			log.Println("error in starting the DataBase: ", err)
 			return
 		}
+		err = CheckMaster()
+		if err != nil {
+			log.Print(err)
+			return
+		}
 		close(done)
 	}()
+}
+
+func CheckMaster() error {
+	masterRepo, err := dallayer.NewMasterDalRequest()
+	if err != nil {
+		log.Print("error in creation of repo")
+		return err
+	}
+	masterId, err := masterRepo.FindAll()
+	if err != nil {
+		log.Print("error in finding the master id details")
+		return err
+	}
+	if len(masterId) == 0 {
+		adminService, err := services.NewAdminService()
+		if err != nil {
+			log.Println("admin service instance starting failure: " + err.Error())
+			return err
+		}
+		newAdminError := adminService.Create()
+		if newAdminError != nil {
+			return errors.New("errors while creating the admin user")
+		}
+	}
+	return nil
 }
 
 func Server() {
@@ -47,7 +79,7 @@ func Server() {
 		})
 	})
 	var Log *log.Logger
-	
+
 	ser, err := services.NewMasterServiceRequest()
 	if err != nil {
 		log.Println("master service instance starting failure: " + err.Error())
